511e788c66f9df8aa07c08f18248030a9af2299f
Kezota <keziameilany2005@gmail.com>
2025-11-11 18:46:43 +0700

    sesi6

--- app/src/main/java/com/example/gettingstartedapp/MainActivity.java
+++ app/src/main/java/com/example/gettingstartedapp/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.gettingstartedapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvHelloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvHelloWorld = findViewById(R.id.tvHelloWorld);
+        registerForContextMenu(tvHelloWorld);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("ini context menu");
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.action_help){
+            Toast.makeText(this, "Context menu help diklik", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        else if(item.getItemId() == R.id.action_settings){
+            Intent niatPindahinSettings = new Intent(this, SettingsActivity.class);
+            startActivity(niatPindahinSettings);
+            Toast.makeText(this, "context menu setting diklik", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.action_help){
+            Toast.makeText(this, "help diklik", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        else if(item.getItemId() == R.id.action_settings){
+            Toast.makeText(this, "setting diklik", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
